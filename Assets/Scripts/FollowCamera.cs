4229b0be9170507ebb5fb6fa453a921a69e9498e
Jonas <jonas-rah@live.com>
2018-02-07 10:24:08 +0100

    start

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public GameObject target;
+    [Range(0.1f, 1f)] public float rotateSpeed = 1f;
+    public float offset = 5f;
+     
+    void LateUpdate() {
+        Vector3 desiredPosition = target.transform.position - transform.forward * offset;
+        desiredPosition +=  transform.right * Input.GetAxis("Mouse X") * rotateSpeed;
+        desiredPosition += transform.up * Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (Mathf.Abs(desiredPosition.y) > offset) return;
+        transform.position = desiredPosition;
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+	public float rollSpeed = 1;
+
+	private Rigidbody wheelRigidbody;
+	// Use this for initialization
+	void Start ()
+	{
+		wheelRigidbody = GetComponent<Rigidbody>();
+		wheelRigidbody.maxAngularVelocity = 500;
+	}
+
+	private void FixedUpdate()
+	{
+		float scroll = Input.GetAxis("ScrollWheel") * rollSpeed;
+		Vector3 camDir = Camera.main.transform.forward;
+		camDir.y = 0;
+		camDir = camDir.normalized;
+		
+		
+		Vector3 rotation = transform.rotation.eulerAngles;
+		Quaternion camLookRot = Quaternion.LookRotation(camDir);
+		float rotate = camLookRot.eulerAngles.y;
+		Quaternion targetRotation = Quaternion.Euler(rotation.x, rotate, 0);
+		//transform.Rotate(0, rotate * Time.fixedDeltaTime * 5, 0);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * 30);
+		
+		if (Mathf.Abs(scroll) > 0f)
+		{
+			
+			wheelRigidbody.AddTorque(transform.right * scroll, ForceMode.VelocityChange);
+		}
+		else
+		{
+			wheelRigidbody.angularVelocity = Vector3.Lerp(wheelRigidbody.angularVelocity, Vector3.zero, Time.fixedDeltaTime * 30);
+		}
+		wheelRigidbody.angularVelocity = transform.right * wheelRigidbody.angularVelocity.magnitude; // add backwards
+	}
+}
