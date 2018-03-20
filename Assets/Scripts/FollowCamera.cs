1d0be2586e74dea190409432f0c7736da5b44bd8
Jonas <jonas-rah@live.com>
2018-03-20 09:26:44 +0100

    new scene

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -9,42 +9,48 @@ public class FollowCamera : MonoBehaviour
     [Range(0.1f, 1f)] public float rotateSpeed = 1f;
     public float distance = 5f;
     private float counter;
-    private SphereCollider col;
     private float tolerance = 0.2f;
-    private float verticalAmount;
     private Vector3 clippingPoint;
+    public LayerMask mask;
 
     private void Start()
     {
-        col = GetComponent<SphereCollider>();
-        col.radius = distance / 2;
         Screen.lockCursor = true;
 
     }
 
+    private void Update() {
+        counter = 0;
+        
+        RaycastHit hit;
+        Ray cameraPole = new Ray(target.transform.position, transform.position - target.transform.position);
+        Debug.DrawRay(cameraPole.origin, cameraPole.direction, Color.red, 0.1f);
+        if (Physics.Raycast(cameraPole, out hit, distance, mask)) {
+            counter = distance - hit.distance;
+        }
+        
+        
+    }
+
     void LateUpdate() {
-        float offset = distance - Mathf.Pow(counter, 2) / distance;
-        verticalAmount += Input.GetAxis("Mouse Y");
+        
+        
+        float offset = distance - counter;
         Vector3 desiredPosition = target.transform.position - transform.forward * offset;
         desiredPosition +=  transform.right * Input.GetAxis("Mouse X") * rotateSpeed;
         float verticalFactor = 1 - counter / distance;
         desiredPosition += transform.up * Input.GetAxis("Mouse Y") * rotateSpeed * verticalFactor;
-        if (desiredPosition.y < clippingPoint.y + tolerance || desiredPosition.y - target.transform.position.y > 4.3f) return;
+        
+        
         transform.position = desiredPosition;
         transform.LookAt(target.transform);
-        
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        clippingPoint = other.ClosestPoint(transform.position);
-        float distanceToPoint = Mathf.Max(Vector3.Distance(transform.position, clippingPoint), distance / 10);
-        counter = (col.radius - distanceToPoint) * 2;
-        Debug.DrawRay(clippingPoint, Vector3.up * counter, Color.red);
+    private float CurvedDistance(float c) {
+        float multiplier = 2;
+        return Mathf.Pow(distance - c, multiplier) / distance;
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        counter = 0;
-    }
+    
+    
 }
