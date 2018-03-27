d528e8a93233cdecdb2065a64d517e875ddd5474
Jonas <jonas-rah@live.com>
2018-03-27 16:54:07 +0200

    this looks nice

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+
+	public float variation = 0;
+
+	private Light _light;
+
+	private float baseLine;
+	// Use this for initialization
+	void Start ()
+	{
+		_light = GetComponent<Light>();
+		baseLine = _light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float targetIntensity = Random.Range(baseLine - variation, baseLine + variation);
+		_light.intensity = Mathf.Lerp(_light.intensity, targetIntensity, Time.deltaTime / 2);
+	}
+}
