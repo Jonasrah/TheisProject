1cc951fbf538c1cdc2c525d6c8a5a9e67b655b90
Jonas <jonas-rah@live.com>
2018-04-15 22:04:40 +0200

    doorway

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	private ResetPosition rp;
+	
+	private void Start() {
+		rp = FindObjectOfType<ResetPosition>();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		rp.e_OutOfBounds.Invoke();
+	}
+}
---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResetPosition : MonoBehaviour {
+	public UnityEvent e_OutOfBounds;
+
+	public Transform spawn;
+
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+
+		if (spawn == null) {
+			Debug.Log("No spawntransform");
+			return;
+		}
+		
+		e_OutOfBounds = new UnityEvent();
+		e_OutOfBounds.AddListener(AllocatePosition);
+	}
+	
+
+	void AllocatePosition () {
+		player.position = spawn.position;
+	}
+}
